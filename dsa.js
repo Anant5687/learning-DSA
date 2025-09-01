36cbdfbd2d0f5396c5e94b1ed6f939cc973f08b7
Anant Jindal <anant.jindal@kredmint.com>
2025-09-01 09:23:00 +0530

    Started with strings

--- dsa.js
+++ dsa.js
@@ -376,3 +376,15 @@ function throttle(fn, delay) {
     lastCall = now;
   };
 }
+
+// 21. Reverse a string in place
+
+function reverseString(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return { newStr };
+}
+
+console.log(reverseString('React'));
